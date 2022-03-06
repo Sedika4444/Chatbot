11af6ae0c2cf341e682a66f3ec9acc6dd8dd9dbe
Irbum <100826416+Irbum@users.noreply.github.com>
2022-03-07 00:49:13 +0800

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -22,7 +22,7 @@ if correct_info and guests[room].lower()==name.lower():
     else:
         correct_info = False
         #Starter Order
-        starters = ['Buuz','Huushur','Tsuivan']
+        starters = ['salad','Huushur','buuz']
         print("For our starters we have:")
         for x in range(len(starters)):
             print(starters[x])
@@ -37,4 +37,39 @@ print(f"You have ordered{starters[1]as your starter")
 elif starter.lower() ==
 starters[2].lower():
 print(f"You have ordered{starters[2]}as your starter")
+
+# Main Course order main = ['Tsuivan','khorkhog','lavhsa',]
+print("Our main dishes:") 
+for x in range(len(main)):
+print(main[x])
+main_dish = input("please type your main dish:")
+if main_dish.lower()== main[0].lower():
+print(f"you have ordered {main[1]} as your main dish")
+elif main_dish.lower()==main[2].lower():
+print(f"you havem ordered{main[2]} as your main dish")
+
+# Dessert Order
+desserts = ['Icecream sundae', 'Chocolate Cake', 'apple pie']
+print("For our dessert dishes we have:")
+for x in range(len(desserts)):
+    print(desserts[x])
+	    
+dessert = input("Please type your main dish: ")
+if dessert.lower() == desserts[0].lower():
+    print(f"You have ordered {desserts[0]} as your dessert")
+elif dessert.lower() == desserts[1].lower():
+    print(f"You have ordered {desserts[1]} as your dessert")
+elif dessert.lower() == desserts[2].lower():
+    print(f"You have ordered {desserts[2]} as your dessert")
+
+#final order
+print(f"Here is your final order: {starter}, {main_dish}, {dessert}")
+
+#confirm order
+order_confirm = input("Do you accept this order?")
+
+if order_confirm.lower() == 'yes':
+    print("Thank you for ordering using our chatbot service! Your food will arrive soon!")
+else:
+    print("Please start your order again.")
 
